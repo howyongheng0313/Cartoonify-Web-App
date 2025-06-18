d5460b5c82c0f54aa85bc34073b4bab40460bec2
HOW YONG-HENG <howyongheng0313@gmail.com>
2025-06-18 16:06:20 +0800

    setup env for deployment

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://localhost:3000", "http://192.168.0.4:3000"],
+    allow_origins=["http://localhost:3000", "https://localhost:3000", "http://192.168.0.4:3000", "https://cartoonifyapp.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
